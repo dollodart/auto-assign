9249a1036f7d00d4a3cb1a7d0ac55660c6e07f4a
david <davidollodart@gmail.com>
2020-09-25 20:23:11 -0700

    add texfmt function and autofmt suffix for arrays
    users can access array values for any indices and do custom formatting
    of arrays, e.g., $\begin{pmatrix} {x[0]}, {x[1]} \end{pmatrix}^T$. But
    user may want autoformatting for convenience, and it would be necessary
    if future implementations allow randomized array size/shapes.
    
    autoformatting is implemented in the template, which should allow these
    assignments to be independent of the document type used (e.g., html
    webpage templates for online access could also be made).

--- dbprobs.py
+++ dbprobs.py
@@ -8,60 +8,91 @@ def solver(x,y):
     return x + y,
 
 P01 = Problem(title = 'Sum Two Numbers',
-        statement = 'Sum the numbers {x} and {y}. '
-        'Here is another number for fun: {z}.',
+        statement = 'Sum the numbers ${x}$ and ${y}$. '
+        'Here is another number for fun: ${z}$.',
         difficulty = 'easy',
         points = 2,
         inputs = (Integer('x',1,10),Integer('y',1,10)),
         extraneous_inputs = (Integer('z',-100,100),),
         solver = solver,
         outputs = ('w',),
-        solution = 'The sum of {x} and {y} is {w}'
+        solution = 'The sum of ${x}$ and ${y}$ is ${w}$'
         )
 
 def solver(x,y):
     return x - y,
 
 P02 = Problem(title = 'Subtract Two Numbers',
-        statement = 'Subtract the number {y} from {x}. '
-        'Here is another number for fun: {z}.',
+        statement = 'Subtract the number ${y}$ from ${x}$. '
+        'Here is another number for fun: ${z}$.',
         difficulty = 'easy',
         points = 2,
         inputs = (Integer('x',1,10),Integer('y',1,10)),
         extraneous_inputs = (Integer('z',-100,100),),
         solver = solver,
         outputs = ('w',),
-        solution = 'The difference of {x} and {y} is {w}'
+        solution = 'The difference of ${x}$ and ${y}$ is ${w}$'
         )
 
 def solver(x,y):
     return x*y,
 
 P03 = Problem(title = 'Multiply Two Numbers',
-        statement = 'Multiply the numbers {x} and {y}. '
-        'Here is another number for fun: {z}.',
+        statement = 'Multiply the numbers ${x}$ and ${y}$. '
+        'Here is another number for fun: ${z}$.',
         difficulty = 'easy',
         points = 2,
         inputs = (Integer('x',1,10),Integer('y',1,10)),
         extraneous_inputs = (Integer('z',-100,100),),
         solver = solver,
         outputs = ('w',),
-        solution = 'The product of {x} and {y} is {w}'
+        solution = 'The product of ${x}$ and ${y}$ is ${w}$'
         )
 
 def solver(x,y):
     return x / y,
 
 P04 = Problem(title = 'Divide Two Numbers',
-        statement = 'Divide the number {x} by {y}. '
-        'Here is another number for fun: {z}.',
+        statement = 'Divide the number ${x}$ by ${y}$. '
+        'Here is another number for fun: ${z}$.',
         difficulty = 'easy',
         points = 2,
         inputs = (Integer('x',1,10),Integer('y',1,10)),
         extraneous_inputs = (Integer('z',-100,100),),
         solver = solver,
         outputs = ('w',),
-        solution = 'The quotient of {x} and {y} is {w}'
+        solution = 'The quotient of ${x}$ and ${y}$ is ${w}$'
         )
 
-A01 = Assignment( (P01,P02,P03,P04), title='Arithmetic Operations')
+from numpy import cumsum
+def solver(x):
+    return cumsum(x),
+
+P05 = Problem(title = 'Sum Several Numbers Cumulatively',
+        statement = 'Sum the numbers in $${x_autofmt}$$ cumulatively top to bottom.'
+        ' The third element of this array is ${x[2]}$.',
+        difficulty = 'medium',
+        points = 4,
+        inputs = (Integer('x',1,10,size=6),),
+        extraneous_inputs = tuple(),
+        solver=solver,
+        outputs= ('w',),
+        solution = 'The cumulative sum is $$\\text{{cumsum}}({x_autofmt}) = {w_autofmt}\,.$$'
+        )
+
+def solver(x):
+    return cumsum(x,axis=0),
+
+P06 = Problem(title = 'Sum Along Several Columns Cumulatively',
+        statement = 'Sum along the columns in $${x_autofmt}$$ cumulatively top to bottom.'
+        ' The element in the second row and third column of this array is ${x[1][2]}$.',
+        difficulty = 'hard',
+        points = 6,
+        inputs = (Integer('x',1,10,size=(6,6)),),
+        extraneous_inputs = tuple(),
+        solver=solver,
+        outputs = ('w',),
+        solution = 'The cumulative sum is $$\\text{{cumsum}}({x_autofmt}) = {w_autofmt}\,.$$'
+        )
+#A01 = Assignment( (P01,P02,P03,P04), title='Arithmetic Operations')
+A01 = Assignment( (P05,P06,P02), title='Several Arithmetic Operations')
