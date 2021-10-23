cb03ef3b13f296e9bee997c5c91a58347f144f08
david <davidollodart@gmail.com>
2021-10-23 13:40:09 -0700

    change compile.py from test file to source file
    source of a 'compiling' function.

--- compile.py
+++ compile.py
@@ -1,38 +1,40 @@
 import subprocess
 import numpy as np
 from Cheetah.Template import Template
-from dbprobs import A01
-
 
 with open('template.cheetah','r') as _:
     tclass = Template.compile(_.read(), baseclass=dict)
 
-# randomly generate possible subset of problems
-A01.rng(len(A01)) 
+def output2latex(assignment, number_students):
+    for n in range(number_students): # number of students
+        name_a = f'out/id-{n}.tex'
+        name_s = f'out/soln-id-{n}.tex'
+        st_a = ''
+        st_s = ''
+        for prob in assignment.subset:
+            for var in prob:
+                var.rng()
+            prob.solve()
 
-for n in range(1): # number of students
-    name_a = f'out/id-{n}.tex'
-    name_s = f'out/soln-id-{n}.tex'
-    st_a = ''
-    st_s = ''
-    for prob in A01.subset:
-        for var in prob:
-            var.rng()
-        prob.solve()
+        with open(name_a, 'w') as _:
+            ass = tclass(assignment_name=assignment.title,
+                    student_id=n,
+                    problems=assignment.subset,
+                    is_soln=False)
+            _.write(ass.respond())
 
-    with open(name_a, 'w') as _:
-        ass = tclass(assignment_name=A01.title,
-                student_id=n,
-                problems=A01.subset,
-                is_soln=False)
-        _.write(ass.respond())
+        with open(name_s, 'w') as _:
+            soln = tclass(assignment_name=assignment.title,
+                    student_id=n,
+                    problems=assignment.subset,
+                    is_soln=True)
+            _.write(soln.respond())
 
-    with open(name_s, 'w') as _:
-        soln = tclass(assignment_name=A01.title,
-                student_id=n,
-                problems=A01.subset,
-                is_soln=True)
-        _.write(soln.respond())
+        subprocess.run(['pdflatex', '-output-directory=out', name_a])
+        subprocess.run(['pdflatex', '-output-directory=out', name_s])
 
-    subprocess.run(['pdflatex', '-output-directory=out', name_a])
-    subprocess.run(['pdflatex', '-output-directory=out', name_s])
+if __name__ == '__main__':
+    from dbprobs import A01
+    # randomly generate possible subset of problems
+    A01.rng(len(A01)) 
+    output2latex(A01, 1)
