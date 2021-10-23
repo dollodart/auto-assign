ddc8b7595eb1aab40c5a8ec447a44826eeef5abf
david <davidollodart@gmail.com>
2021-10-23 14:35:42 -0700

    added test of classes
    the classes defined in problem.py will have to be refactored, as they
    are not consistent in inheritance or function. also fixed a bug in the
    from unit with dimensionality in which an ndarray was tried to be
    hashed and needed to be converted to a tuple

--- test.py
+++ test.py
@@ -30,7 +30,40 @@ def test_all_output2pdf():
     for ass in assignments:
         output2pdf(ass, 1)
 
+from problem import *
+
+def test_RandomUnit():
+    print('random unit from dimensionality of SI unit of energy')
+    ru = RandomUnit.from_unit_dimensionality('kg*m^2*s^-2')
+    print('random unit "value"')
+    print(ru.value)
+    print('random unit unit set consistent with dimensionality')
+    print(ru.unit_set)
+    ru.rng()
+    print('random unit "value" after rng')
+    print(ru.value)
+    print('random unit conversion factor from previous to current value')
+    print(ru.conversion_factor)
+
+def test_RandomQuantity():
+    print('random quantity test')
+    print('mass over length then squared')
+    rsize = RandomSize( ((3,4),) )
+    runit = RandomUnit(['kg/m','lb/ft','g/cm'])
+    rq = RandomQuantity('rq',2,5,rsize,runit,3)
+    res = rq.value*rq.value
+    print(res)
+
+def test_ConstantFloat():
+    print('constant float test')
+    rq2 = ConstantFloat('a', 2350)
+    print(rq2.value)
+
 if __name__ == '__main__':
-    test_all_assignments()
-    test_all_problems()
-    test_all_output2latex()
+    #test_all_assignments()
+    #test_all_problems()
+    #test_all_output2latex()
+    
+    test_RandomUnit()
+    test_RandomQuantity()
+    test_ConstantFloat()
